29aaa6f59816759716df7be6ed1bc8b1ac46b086
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-09-19 15:00:15 +0100

    la 'ajout des block try catch pour la gestion des exception

--- Gestion-CompteBancaire/src/com/Controller/BanqueController.java
+++ Gestion-CompteBancaire/src/com/Controller/BanqueController.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import com.Models.*;
 
 import com.util.Utils;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,117 +64,167 @@ public class BanqueController {
     }
 
     private void creerCompteCourant() {
-        System.out.print("Code (CPT-12345):");
-        String code = sc.nextLine().trim();
-        if (!Utils.isValidCode(code)) {
-            System.out.println("Code invalide");
-            return;
+        try {
+            System.out.print("Code (CPT-12345):");
+            String code = sc.nextLine().trim();
+            if (!Utils.isValidCode(code)) {
+                System.out.println("Code invalide");
+                return;
+            }
+            System.out.print("Solde initial : ");
+            float solde = Utils.lireFloat(sc.nextLine().trim());
+            System.out.print("Découvert autorisé:");
+            double dec = Utils.lireFloat(sc.nextLine().trim());
+            comptes.put(code, new CompteCourant(code, solde, dec));
+            System.out.println("Compte courant créé .");
+
         }
-        System.out.print("Solde initial");
-        float solde = Utils.lireFloat(sc.nextLine().trim());
-        System.out.print("Découvert autorisé:");
-        double dec = Utils.lireFloat(sc.nextLine().trim());
-        comptes.put(code, new CompteCourant(code, solde, dec));
-        System.out.println("Compte courant créé .");
+        // le lancement d'une exception dans le cas ou l'utilisatuer saisi une autre chose qu'un nombre .
+        catch (NumberFormatException e) {
+            System.out.println("Erreur : veuillez entrer un nombre valide.");
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue : " + e.getMessage());
+        }
     }
 
     private void creerCompteEpargne() {
-        System.out.print("Code (CPT-12345):");
-        String code = sc.nextLine().trim();
-        if (!Utils.isValidCode(code)) {
-            System.out.println("Code invalide");
-            return;
+        try {
+            System.out.print("Code (CPT-12345):");
+            String code = sc.nextLine().trim();
+            if (!Utils.isValidCode(code)) {
+                System.out.println("Code invalide");
+                return;
+            }
+            System.out.print("Solde initial");
+            float solde = Utils.lireFloat(sc.nextLine().trim());
+            //à ajouter le taux d'intéret doit etre ajouter pour chaque 5secondes
+            System.out.print("Taux intéret:");
+            double taux = Utils.lireFloat(sc.nextLine().trim());
+            comptes.put(code, new CompteEpargne(code, solde, taux));
+            System.out.println("Compte épargne créé");
         }
-        System.out.print("Solde initial");
-        float solde = Utils.lireFloat(sc.nextLine().trim());
-        System.out.print("Taux intéret:");
-        double taux = Utils.lireFloat(sc.nextLine().trim());
-        comptes.put(code, new CompteEpargne(code, solde, taux));
-        System.out.println("Compte épargne créé");
 
+        // le lancement d'une exception dans le cas ou l'utilisatuer saisi une autre chose qu'un nombre .
+        catch (NumberFormatException e) {
+            System.out.println("Erreur : veuillez entrer un nombre valide.");
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue : " + e.getMessage());
+        }
     }
 
     private void faireVersement() {
-        System.out.print("Code compte :");
-        String code = sc.nextLine().trim();
-        Compte c = comptes.get(code);
-        if (c == null) {
-            System.out.println("Compte introuvable");
-            return;
+        try {
+            System.out.print("Code compte :");
+            String code = sc.nextLine().trim();
+            Compte c = comptes.get(code);
+            if (c == null) {
+                System.out.println("Compte introuvable");
+                return;
+            }
+            System.out.print("Montant:");
+            float montant = Utils.lireFloat(sc.nextLine().trim());
+            System.out.print("Source:");
+            String source = sc.nextLine().trim();
+            c.setSolde(c.getSolde() + montant);
+            c.addOperation(new Versement(montant, source));
+            System.out.println("Versement effectué .");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Erreur : le montant doit être un nombre.");
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue : " + e.getMessage());
         }
-        System.out.print("Montant:");
-        float montant = Utils.lireFloat(sc.nextLine().trim());
-        System.out.print("Source:");
-        String source = sc.nextLine().trim();
-        c.setSolde(c.getSolde() + montant) ;
-        c.addOperation(new Versement(montant, source));
-        System.out.println("Versement effectué .");
     }
 
     private void faireRetrait() {
-        System.out.print("Code compte :");
-        String code = sc.nextLine().trim();
-        Compte c = comptes.get(code);
-        if (c == null) {
-            System.out.println("Compte introuvable");
-            return;
-        }
-        System.out.print("Montant: ");
-        float montant = Utils.lireFloat(sc.nextLine().trim());
-        System.out.print("Destination:");
-        String dest = sc.nextLine().trim();
-        if (!c.retirer(montant)) {
-            System.out.println("Retrait impossible");
-            return;
-        }
-        c.addOperation(new Retrait(montant, dest));
-        System.out.println("Retrait effectué .");
+        try {
+            System.out.print("Code compte :");
+            String code = sc.nextLine().trim();
+            Compte c = comptes.get(code);
+            if (c == null) {
+                System.out.println("Compte introuvable");
+                return;
+            }
+            System.out.print("Montant: ");
+            float montant = Utils.lireFloat(sc.nextLine().trim());
+            System.out.print("Destination:");
+            String dest = sc.nextLine().trim();
+            if (!c.retirer(montant)) {
+                System.out.println("Retrait impossible");
+                return;
+            }
+            c.addOperation(new Retrait(montant, dest));
+            System.out.println("Retrait effectué .");
 
+        } catch (NumberFormatException e) {
+            System.out.println("Erreur : le montant doit être un nombre.");
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue : " + e.getMessage());
+        }
     }
 
     private void faireVirement() {
-        System.out.print("Compte source :");
-        String src = sc.nextLine().trim();
-        System.out.print("Compte cible :");
-        String tgt = sc.nextLine().trim();
-        Compte from = comptes.get(src);
-        Compte to = comptes.get(tgt);
-        if (from == null || to == null) {
-            System.out.println("Compte introuvable");
-            return;
+        try {
+            System.out.print("Compte source :");
+            String src = sc.nextLine().trim();
+            System.out.print("Compte cible :");
+            String tgt = sc.nextLine().trim();
+            Compte from = comptes.get(src);
+            Compte to = comptes.get(tgt);
+            if (from == null || to == null) {
+                System.out.println("Compte introuvable");
+                return;
+            }
+            System.out.print("Montant:");
+            float montant = Utils.lireFloat(sc.nextLine().trim());
+            if (!from.retirer(montant)) {
+                System.out.println("Virment impossible");
+                return;
+            }
+            to.setSolde(to.getSolde() + montant);
+            from.addOperation(new Retrait(montant, "Virment vers" + tgt));
+            to.addOperation(new Versement(montant, "Virment depuis" + src));
+            System.out.println("Virment effectué.");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Erreur : le montant doit être un nombre.");
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue : " + e.getMessage());
         }
-        System.out.print("Montant:");
-        float montant = Utils.lireFloat(sc.nextLine().trim());
-        if (!from.retirer(montant)) {
-            System.out.println("Virment impossible");
-            return;
-        }
-        to.setSolde(to.getSolde() + montant);
-        from.addOperation(new Retrait(montant, "Virment vers" + tgt));
-        to.addOperation(new Versement(montant, "Virment depuis" + src));
-        System.out.println("Virment effectué.");
     }
 
     private void consulterSolde() {
-        System.out.print("Code compte :");
-        String code = sc.nextLine().trim();
-        Compte c = comptes.get(code);
-        if (c == null) {
-            System.out.println("Compte introuvable");
-            return;
+        try {
+            System.out.print("Code compte :");
+            String code = sc.nextLine().trim();
+            Compte c = comptes.get(code);
+            if (c == null) {
+                System.out.println("Compte introuvable");
+                return;
+            }
+            c.afficherDetails();
+
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue : " + e.getMessage());
         }
-        c.afficherDetails();
     }
 
     private void listerOperations() {
-        System.out.print("Code compte :");
-        String code = sc.nextLine().trim();
-        Compte c = comptes.get(code);
-        if (c == null) {
-            System.out.println("Compte introuvable");
-            return;
+
+        try {
+            System.out.print("Code compte :");
+            String code = sc.nextLine().trim();
+            Compte c = comptes.get(code);
+            if (c == null) {
+                System.out.println("Compte introuvable");
+                return;
+            }
+            if (c.getListOperation().isEmpty()) System.out.println("Acune opération");
+            else c.getListOperation().forEach(System.out::println);
+
+            // c'est équivalen à écrire c.getListOperation().forEach(op->System.out.println(op));
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue : " + e.getMessage());
         }
-        if (c.getListOperation().isEmpty()) System.out.println("Acune opération");
-        else c.getListOperation().forEach(System.out::println);
     }
 }
