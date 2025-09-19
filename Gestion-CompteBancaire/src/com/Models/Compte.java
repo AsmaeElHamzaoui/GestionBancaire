8b52bfaf47f5b13e0660b267c04e504f3c8c965f
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-09-19 09:18:02 +0100

    L'implémentation de la classe Compte avec les attribues les getters et setters anisi que la signature des méthodes abstraites

---
+++ Gestion-CompteBancaire/src/com/Models/Compte.java
@@ -0,0 +1,52 @@
+package com.Models;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Compte {
+    private String code ;
+    private float solde;
+    private List<Operation> listOperation;
+
+
+    public Compte(String code, float solde){
+        this.code=code;
+        this.solde=solde;
+        this.listOperation=new ArrayList<>();
+    }
+
+    //Getters et setters (begin)
+
+    public String getCode(){
+        return code;
+    }
+    public void setCode(String code){
+        this.code=code;
+    }
+    public float getSolde(){
+        return solde;
+    }
+    public void setSolde(float solde){
+        this.solde=solde;
+    }
+    public List<Operation> getListOperation(){
+        return listOperation;
+    }
+    public void  setListOperation(List<Operation> listOperation){
+        this.listOperation=listOperation;
+    }
+
+    //Getters et setters(end)
+
+    //Méthodes abstraites
+    public abstract boolean retirer(float montant);
+
+    public abstract double calculerInteret();
+
+    public abstract void afficherDetails();
+
+    //Méthode pour ajouter une opération
+    public void addOperation(Operation op) {
+        this.listOperation.add(op);
+    }
+}
