410c43d3505a7f7858744fef146b02be3a0058d8
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-09-19 09:23:46 +0100

    L'implémentation de la class main pour manipuler le projet

---
+++ Gestion-CompteBancaire/src/Main.java
@@ -0,0 +1,12 @@
+
+
+import com.Controller.BanqueController;
+
+import static java.lang.System.out;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        BanqueController controller=new BanqueController();
+        controller.commencer();
+    }
+}
